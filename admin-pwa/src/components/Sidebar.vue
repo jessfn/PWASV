0511da10005eb93711b40a628969f53d895c820c
Jesus200995 <jesus.riosgomez@utxicotepec.edu.mx>
2025-12-05 14:38:01 -0600

    feat: Eliminar ícono de territorio en Sidebar para simplificar visualización

--- admin-pwa/src/components/Sidebar.vue
+++ admin-pwa/src/components/Sidebar.vue
@@ -122,9 +122,6 @@
               <div v-if="isTerritorial && territorioAsignado" class="territorio-separator"></div>
               <!-- Territorio integrado debajo -->
               <div v-if="isTerritorial && territorioAsignado" class="user-territorio">
-                <svg class="user-territorio-icon" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-                </svg>
                 <span class="user-territorio-text">{{ territorioAsignado }}</span>
               </div>
               <p v-if="isTerritorial && estadosDelTerritorio" class="user-estados">{{ estadosDelTerritorio }}</p>
