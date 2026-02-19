379e76fa845c8be6fb3ae4edeb289b12a941e698
Jesus200995 <jesus.riosgomez@utxicotepec.edu.mx>
2026-02-19 17:41:14 -0600

    feat: Mostrar iconos de editar y eliminar en permiso registros_acciones

--- admin-pwa/src/views/PermisosView.vue
+++ admin-pwa/src/views/PermisosView.vue
@@ -740,13 +740,19 @@
                   <div v-if="formularioUsuario.permisos.registros" class="sub-permiso-container">
                     <label class="sub-permiso-item" :class="{ 'active': formularioUsuario.permisos.registros_acciones }">
                       <input type="checkbox" v-model="formularioUsuario.permisos.registros_acciones" />
-                      <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                        <polyline points="3 6 5 6 21 6"/>
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
-                        <line x1="10" y1="11" x2="10" y2="17"/>
-                        <line x1="14" y1="11" x2="14" y2="17"/>
-                      </svg>
-                      <span class="sub-permiso-text">Permitir eliminar</span>
+                      <div class="sub-permiso-icons">
+                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                          <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                          <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                        </svg>
+                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" style="margin-left: 4px;">
+                          <polyline points="3 6 5 6 21 6"/>
+                          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+                          <line x1="10" y1="11" x2="10" y2="17"/>
+                          <line x1="14" y1="11" x2="14" y2="17"/>
+                        </svg>
+                      </div>
+                      <span class="sub-permiso-text">Permitir editar y eliminar</span>
                       <div class="sub-toggle">
                         <div class="sub-toggle-track">
                           <div class="sub-toggle-thumb"></div>
@@ -2917,6 +2923,12 @@ export default {
 
 .sub-permiso-item input[type="checkbox"] {
   display: none;
+}
+
+.sub-permiso-icons {
+  display: flex;
+  align-items: center;
+  gap: 4px;
 }
 
 .sub-permiso-item svg {
